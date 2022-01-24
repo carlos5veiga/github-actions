5f2947e2a20eef7716aebee362a961d9fbe0e404
carlos5veiga <carlos5veiga@gmail.com>
2022-01-24 19:40:03 -0300

    DTO

--- backend/src/main/java/com/carlosveigafilho/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/carlosveigafilho/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.carlosveigafilho.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.carlosveigafilho.dscatalog.dto.CategoryDTO;
 import com.carlosveigafilho.dscatalog.entities.Category;
 import com.carlosveigafilho.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly=true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
 	}
 }
