4515bd7823e4afcdc8376393fb25a51e727fb2f5
carlos5veiga <carlos5veiga@gmail.com>
2022-02-03 19:54:55 -0300

    Service tests

--- backend/src/test/java/com/carlosveigafilho/dscatalog/tests/Factory.java
+++ backend/src/test/java/com/carlosveigafilho/dscatalog/tests/Factory.java
@@ -2,6 +2,7 @@ package com.carlosveigafilho.dscatalog.tests;
 
 import java.time.Instant;
 
+import com.carlosveigafilho.dscatalog.dto.ProductDTO;
 import com.carlosveigafilho.dscatalog.entities.Category;
 import com.carlosveigafilho.dscatalog.entities.Product;
 
@@ -9,7 +10,16 @@ public class Factory {
 
 	public static Product createProduct() {
 		Product product = new Product(1L, "Test PC", "Test Description", 40.00, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg", Instant.now());
-		product.getCategories().add(new Category(2L, "Electronics"));
+		product.getCategories().add(createCategory());
 		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+	public static Category createCategory() {
+		return new Category(2L, "Electronics");
 	}
 }
