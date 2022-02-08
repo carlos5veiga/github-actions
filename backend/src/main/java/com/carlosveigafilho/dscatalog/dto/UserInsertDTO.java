dc3bb7c7d894596c42aa9fec7b510d657ed486d5
carlos5veiga <carlos5veiga@gmail.com>
2022-02-08 16:14:55 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/carlosveigafilho/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/carlosveigafilho/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.carlosveigafilho.dscatalog.dto;
 
+import com.carlosveigafilho.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
 	private static final long serialVersionUID = 1L;
